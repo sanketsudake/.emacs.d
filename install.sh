72fe3c844c78ea16d72c9a473a28fe16c4e7368e
Sanket Sudake <sanketsudake@gmail.com>
2013-10-20 01:47:05 +0530

    Updated install.sh for clang req.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install clang libclang-dev git-core&& \
+sudo apt-get install clang-3.4 libclang-3.4-dev git-core python-pip && \
 cd ~/
 git clone https://github.com/tripples/.emacs.d.git ~/.emacs.d/
 cd ~/.emacs.d/
@@ -7,4 +7,4 @@ git submodule init
 git submodule update
 sudo pip install -r requirements.txt && \
 cd ~/.emacs.d/src/clang/ && \
-make
+make LLVM_CONFIG=llvm-config-3.4
