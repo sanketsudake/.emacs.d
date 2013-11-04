f1f0816b8052087b0dc40d99423ab2047e2439f5
Sanket Sudake <sanketsudake@gmail.com>
2013-11-04 15:31:41 +0530

    Add org shortcuts

--- src/set-org.el
+++ src/set-org.el
@@ -3,5 +3,9 @@
 ;; Licence: GNU GPL v3
 
 ;; ToDo: set customized org mode
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
 
 (provide 'set-org)
