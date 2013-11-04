238e05c5b68f74a9a04acc8c687a4673ce46e53a
Sanket Sudake <sanketsudake@gmail.com>
2013-11-04 15:32:20 +0530

    Add windmove keybindings

--- src/set-keybindings.el
+++ src/set-keybindings.el
@@ -3,4 +3,8 @@
 ;; Licence: GNU GPL v3
 
 ;; ToDo: set customized org mode
+(global-set-key (kbd "<M-left>") 'windmove-left)
+(global-set-key (kbd "<M-right>") 'windmove-right)
+(global-set-key (kbd "<M-up>") 'windmove-up)
+(global-set-key (kbd "<M-down>") 'windmove-down)
 (provide 'set-keybindings)
