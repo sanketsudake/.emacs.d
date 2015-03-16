a71e2139d45af98305a9fc466fe21a49d0994d42
Sanket Sudake <sanketsudake@gmail.com>
2015-03-16 04:55:36 -0700

    Reinit for new setup

--- README.md
+++
@@ -1,7 +0,0 @@
-Emacs Config
------------------------
-<b>Setup</b>
-```
-curl -L https://raw.github.com/tripples/.emacs.d/master/install.sh | sh
-```
-
--- set-keybind.el
+++
@@ -1,4 +0,0 @@
-;; ;; Disable Arrow Keys
-;; (mapc
-;;  (lambda (ss-keyval) (global-set-key (kbd ss-keyval) 'disabled-key))
-;;  (list "<up>" "<down>" "<left>" "<right>"))
